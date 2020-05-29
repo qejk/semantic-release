6510c382e71217b0224dcd85f56c928c012952b3
Adam <qejk@qejk.net>
2020-05-29 06:31:12 +0200

    feat: add multiply (#19) (#20)
    * [FOO-102] feat: add multiply (#19)
    
    * feat: add multiply
    
    * refactor: remove unnecessary semantic-release on update-pull-request action
    
    Co-authored-by: Adam Desivi <desivi@desivi.com>
    
    * fix: syntax on action
    
    Co-authored-by: Adam Desivi <desivi@desivi.com>

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ function add(a, b) {
 function subtract(a, b) {
   return a - b;
 }
+function multiply(a, b) {
+  return a * b;
+}
 
-module.exports = {add, subtract};
+module.exports = {add, subtract, multiply};
